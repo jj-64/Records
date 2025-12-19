faf3e03eeafd0fef20f3e308355a07fa98bfa065
Jinane Jouni <jinane.jouni@hotmail.com>
2025-12-19 16:01:50 +0200

    initial commit

---
+++ R/data-feature_matrix.R
@@ -0,0 +1,7 @@
+#' Feature matrix
+#'
+#' A numeric feature matrix used for model comparison.
+#'
+#' @format A data.frame with rows representing observations and columns representing features.
+#' @source Generated internally
+"feature_matrix"
