28dbab5813fc592990a059fe041ad00f346d6dba
Jinane Jouni <jinane.jouni@hotmail.com>
2025-12-19 16:00:14 +0200

    initial commit

---
+++ R/globals.R
@@ -0,0 +1,11 @@
+utils::globalVariables(c(
+  "Decision", "Freq",  "p_value1",
+  "gamma_hat", "n", "rec_count_var_DTRW_approx",
+  "u_t_YNM",
+  "Decision", "Freq", "Permutation", "n",
+  "approxiamte", "approximate",
+  "gamma_hat",
+  "rec_count_var_DTRW_approx",
+  "u_t_YNM", "p_value1",
+  "multiClassSummary"
+))
