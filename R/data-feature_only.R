dcc9162e6ffdd6007a68f2f10a7d04e33a813246
Jinane Jouni <jinane.jouni@hotmail.com>
2025-12-21 11:49:28 +0200

    updated data

---
+++ R/data-feature_only.R
@@ -0,0 +1,7 @@
+#' feature only
+#'
+#' A numeric feature matrix used for model comparison.
+#'
+#' @format A data.frame with rows representing observations and columns representing features.
+#' @source Generated internally
+"feature_only"
